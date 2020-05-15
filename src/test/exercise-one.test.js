be55f38844a361638195589474f047134b112b23
albertosca <albertoalbuquerque01@gmail.com>
2020-05-15 17:15:44 -0300

    Update exercise-one.test.js

--- src/test/exercise-one.test.js
+++ src/test/exercise-one.test.js
@@ -5,16 +5,14 @@ import App from '../App';
 afterEach(cleanup);
 
 // arquivo App.test.js pode servir de exemplo
-describe('Testando a aplicação, testando botão, e sua funcionalidade', () => {
+describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
   test('Verificando se o botão está na tela e se o ele contém o texto "Adicionar"', () => {
     const {getByText} = render(<App />);
 
   });
 
-  test(`
-    Verificando sua funcionalidade, ao ser clicado é necessário adicionar o que o usuário digitou
-  Use os fireEvent, para simular a digitação do usuário e o clique.
-  `, () => {
+  test(`Ao clicar no botão, é necessário adicionar o que o usuário digitou à lista`, () => {
+    // Use os fireEvent, para simular a digitação do usuário e o clique.
     const {getByLabelText,queryByText} = render(<App />);
 
   });
