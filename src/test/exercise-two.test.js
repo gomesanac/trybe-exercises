35975213649f5608cd9274a237361c77e5e14552
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-30 16:07:25 -0300

    exercise-two

--- src/test/exercise-two.test.js
+++ src/test/exercise-two.test.js
@@ -8,13 +8,24 @@ afterEach(cleanup);
 describe('Teste do campo de input', () => {
   test('Testando a adição de vários itens a aplicação', () => {
     const listTodo = ['Realizar CR', 'Ler Post no Medium', 'Beber água']; // Use esse array como base para realizar os testes.
-    const {} = render(<App />) // Caso precise de uma nova query adicione no object destructuring
+    const { getByLabelText, queryByText, getByText } = render(<App />); // Caso precise de uma nova query adicione no object destructuring
+    const input = getByLabelText('Tarefa:');
+    const button = queryByText('Adicionar');
 
+    listTodo.forEach(todo => {
+      fireEvent.change(input, { target: { value: todo }});
+      fireEvent.click(button);
+
+      expect(getByText(todo)).toBeInTheDocument();
+    })
   })
 });
 
 describe('Teste do componente Item', () => {
   test('Ao receber uma string como prop ela precisa aparecer na tela', () => {
+    const todo = 'Realizar CR';
+    const { getByText } = render(<Item content={todo}/>);
 
+    expect(getByText(todo)).toBeInTheDocument();
   })
 })
