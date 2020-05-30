4e41056b7009bd090a9394f15bd611ccf16356ca
Ana Carolina Gomes <gomes.anac03@gmail.com>
2020-05-30 15:45:35 -0300

    exercise-one

--- src/test/exercise-one.test.js
+++ src/test/exercise-one.test.js
@@ -7,13 +7,22 @@ afterEach(cleanup);
 // arquivo App.test.js pode servir de exemplo
 describe('Teste da aplicação, testando o botão e sua funcionalidade', () => {
   test('Verificando se o botão está na tela e se o ele contém o texto "Adicionar"', () => {
-    const {getByText} = render(<App />);
+    const { getByRole } = render(<App />);
+    const button = getByRole('button');
 
+    expect(button).toBeInTheDocument();
+    expect(button.value).toBe('Adicionar');
   });
 
   test(`Ao clicar no botão, é necessário adicionar o que o usuário digitou à lista`, () => {
     // Use os fireEvent, para simular a digitação do usuário e o clique.
-    const {getByLabelText,queryByText} = render(<App />);
+    const { getByLabelText, queryByText } = render(<App />);
+    const input = getByLabelText('Tarefa:');
+    const button = queryByText('Adicionar');
 
+    fireEvent.change(input, { target: { value: 'Estudar' }});
+    fireEvent.click(button);
+
+    expect(queryByText('Estudar')).toBeInTheDocument();
   });
 });
